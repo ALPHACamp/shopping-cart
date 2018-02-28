38552bcc58175040e5067b1568db9ecbfa3f53e8
Ellen Lee <leechinghui.tw@gmail.com>
2018-02-28 13:58:27 +0800

    Setup routes for product (normal user and admin)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "products#index"
+  resources :products, only: [:index, :show]
+
+  namespace :admin do
+    root "products#index"
+    resources :products
+  end
 end
