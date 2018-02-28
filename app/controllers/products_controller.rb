0115fd2c25e0bce1aa96a5fd3cf693ed15e523d5
Ellen Lee <leechinghui.tw@gmail.com>
2018-03-01 02:09:00 +0800

    AJAX add_to_cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     current_cart.add_cart_item(@product)
 
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 
   def remove_from_cart
