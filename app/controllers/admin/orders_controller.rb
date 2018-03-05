c4714bf3facd4f526859dc14d039442ba0a85a0a
Ellen Lee <leechinghui.tw@gmail.com>
2018-03-05 14:22:32 +0800

    user receive notification by email when order payment/shipped status update

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,6 +10,12 @@ class Admin::OrdersController < Admin::AdminController
   def update
     @order = Order.find(params[:id])
     if @order.update(order_params)
+      if @order.shipping_status == "shipped"
+        UserMailer.notify_order_shipped(@order).deliver_now
+      end
+      if @order.payment_status == "paid"
+        UserMailer.notify_order_paid(@order).deliver_now
+      end
       redirect_to admin_orders_path, notice: "Order updated"
     else
       flash.now[:alert] = @order.errors.full_messages.to_sentence
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,6 +6,20 @@ class UserMailer < ApplicationMailer
     @content = "Your order is created. Thank you!"
 
     mail to: order.user.email,
-    subject: "ALPHA Camp | 訂單成立: #{@order.id}"
+    subject: "ALPHA Camp | 訂單成立: #{@order.sn}"
+  end
+
+  def notify_order_paid(order)
+    @order = order
+    @content = "Your order has been paid."
+    mail to: order.user.email,
+    subject: "Order ##{@order.sn} has been paid"
+  end
+
+  def notify_order_shipped(order)
+    @order = order
+    @content = "Your order has been shipped."
+    mail to: order.user.email,
+    subject: "Order ##{@order.sn} has been shipped."
   end
 end
