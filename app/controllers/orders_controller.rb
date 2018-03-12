5ba70a41283b7445232fd6c32b26da321ae15e91
Ellen Lee <leechinghui.tw@gmail.com>
2018-03-13 04:14:02 +0800

    user can choose payment method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -45,6 +45,7 @@ class OrdersController < ApplicationController
       @payment = Payment.create!(
         sn: Time.now.to_i,
         order_id: @order.id,
+        payment_method: params[:payment_method],
         amount: @order.amount
       )
 
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,10 @@
 class Payment < ApplicationRecord
   belongs_to :order
+
+  PAYMENT_METHODS = %w[Credit WebATM ATM]
+  validates_inclusion_of :payment_method, in: PAYMENT_METHODS
+
+  def deadline
+    Date.today + 3.days
+  end
 end
