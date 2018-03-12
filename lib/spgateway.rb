7960f944d7a1667b2b316b9746a42ea252a8724a
Ellen Lee <leechinghui.tw@gmail.com>
2018-03-13 04:14:02 +0800

    user can choose payment method

--- lib/spgateway.rb
+++ lib/spgateway.rb
@@ -27,8 +27,20 @@ class Spgateway
       ItemDesc: @payment.order.name,
       ReturnURL: return_url,
       Email: @payment.order.user.email,
-      LoginType: 0
+      LoginType: 0,
+      CREDIT: 0,
+      WEBATM: 0,
+      VACC: 0
     }
+
+    case @payment.payment_method
+      when "Credit"
+        spgateway_data.merge!( :CREDIT => 1 )
+      when "WebATM"
+        spgateway_data.merge!( :WEBATM => 1 )
+      when "ATM"
+        spgateway_data.merge!( :VACC => 1, :ExpireDate => @payment.deadline.strftime("%Y%m%d") )
+    end
 
     trade_info = self.encrypt(spgateway_data)
     trade_sha = self.class.generate_aes_sha256(trade_info)
