24ede09835d7d700b40e3a2436fdb7fca7f6b318
Ellen Lee <leechinghui.tw@gmail.com>
2018-03-13 03:42:13 +0800

    extract decrypt process to spgateway class

--- app/controllers/spgateway_controller.rb
+++ app/controllers/spgateway_controller.rb
@@ -1,29 +1,10 @@
 class SpgatewayController < ActionController::Base
 
   def return
-    hash_key = "DEOViIHoxZRzElSe9p14KFa8k4vx7Tfv"
-    hash_iv = "nIxiaIldrOFR4JPe"
     trade_info = spagatway_params['TradeInfo']
     trade_sha = spagatway_params['TradeSha']
 
-    str = "HashKey=#{hash_key}&#{trade_info}&HashIV=#{hash_iv}"
-    check_sha = Digest::SHA256.hexdigest(str).upcase
-
-    if check_sha == trade_sha
-      decipher = OpenSSL::Cipher::AES256.new(:CBC)
-      decipher.decrypt
-      decipher.key = hash_key
-      decipher.iv = hash_iv
-
-      binary_encrypted = [trade_info].pack('H*') # hex to binary
-      plain = decipher.update(binary_encrypted)
-
-      # strip last padding
-      if plain[-1] != '}'
-        plain = plain[0, plain.index(plain[-1])]
-      end
-      data = JSON.parse(plain)
-    end
+    data = Spgateway.decrypt(trade_info, trade_sha)
 
     if data
       payment = Payment.find(data['Result']['MerchantOrderNo'].to_i)
