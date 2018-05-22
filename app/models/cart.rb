a3533995c0c03fb662f92c5da5366cbea0b2283e
Ellen Lee <leechinghui.tw@gmail.com>
2018-05-22 13:22:45 +0800

    fix add_cart_item

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,12 +3,12 @@ class Cart < ApplicationRecord
   has_many :products, through: :cart_items
 
   def add_cart_item(product)
-    existing_item = self.cart_items.find_by( product_id: product )
+    existing_item = self.cart_items.find_by( product: product )
     if existing_item
       existing_item.quantity += 1
       existing_item.save!
     else
-      cart_item = cart_items.build( product_id: product.id )
+      cart_item = cart_items.build( product: product )
       cart_item.save!
     end
     self.cart_items
